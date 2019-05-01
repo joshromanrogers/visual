d2607655e51aa00a5d4562c2b7f4b25fe9fce6ef
Josh Rogers <joshromanrogers@gmail.com>
2019-05-01 14:19:08 +0200

    controller + screen positioning sorted

--- index.js
+++ index.js
@@ -8,7 +8,8 @@ var camera = new THREE.PerspectiveCamera( 75, window.innerWidth/window.innerHeig
 camera.position.z = 40;
 
 var renderer = new THREE.WebGLRenderer();
-renderer.setSize( window.innerWidth, window.innerHeight );
+renderer.domElement.id = "canvas";
+renderer.setSize( window.innerWidth/100*70, window.innerHeight/100*70 );
 document.body.appendChild( renderer.domElement );
 
 var geometry = new THREE.DodecahedronGeometry(1, 3);
@@ -27,14 +28,14 @@ var boxes = [];
 var numBoxes = 5;
 
 for (let x = -numBoxes; x <= numBoxes; x++) {
-  for (let y = -numBoxes; y <= numBoxes; y++) {
-    var material = new THREE.MeshStandardMaterial( );
-    var cube = new THREE.Mesh( geometry, material );
-    cube.position.x = x * 2;
-    cube.position.y = y * 2;
-    boxes.push(cube);
-    scene.add(cube);
-  }
+	for (let y = -numBoxes; y <= numBoxes; y++) {
+		var material = new THREE.MeshStandardMaterial( );
+		var cube = new THREE.Mesh( geometry, material );
+		cube.position.x = x * 2;
+		cube.position.y = y * 2;
+		boxes.push(cube);
+		scene.add(cube);
+	}
 }
 
 // initial values
@@ -52,181 +53,181 @@ let colBVal = 1;
 let posZValIn = document.getElementById("shapePosZInput");
 let posZBtn = document.getElementById("shapePosZ");
 posZBtn.addEventListener("click", function() {
-  posZVal = posZValIn.value;
+	posZVal = posZValIn.value;
 });
 
 let scaleZValIn = document.getElementById("shapeScaleZInput");
 let scaleZBtn = document.getElementById("shapeScaleZ");
 scaleZBtn.addEventListener("click", function() {
-  scaleZVal = scaleZValIn.value;
+	scaleZVal = scaleZValIn.value;
 });
 
 let scaleXValIn = document.getElementById("shapeScaleXInput");
 let scaleXBtn = document.getElementById("shapeScaleX");
 scaleXBtn.addEventListener("click", function() {
-  scaleXVal = scaleXValIn.value;
+	scaleXVal = scaleXValIn.value;
 });
 
 let scaleYValIn = document.getElementById("shapeScaleYInput");
 let scaleYBtn = document.getElementById("shapeScaleY");
 scaleYBtn.addEventListener("click", function() {
-  scaleYVal = scaleYValIn.value;
+	scaleYVal = scaleYValIn.value;
 });
 
 let rotationZValIn = document.getElementById("shapeRotationZInput");
 let rotationZBtn = document.getElementById("shapeRotationZ");
 rotationZBtn.addEventListener("click", function() {
-  rotationZVal = rotationZValIn.value;
+	rotationZVal = rotationZValIn.value;
 });
 
 let rotationXValIn = document.getElementById("shapeRotationXInput");
 let rotationXBtn = document.getElementById("shapeRotationX");
 rotationXBtn.addEventListener("click", function() {
-  rotationXVal = rotationXValIn.value;
+	rotationXVal = rotationXValIn.value;
 });
 
 let rotationYValIn = document.getElementById("shapeRotationYInput");
 let rotationYBtn = document.getElementById("shapeRotationY");
 rotationYBtn.addEventListener("click", function() {
-  rotationYVal = rotationYValIn.value;
+	rotationYVal = rotationYValIn.value;
 });
 
 let colRValIn = document.getElementById("shapeColRInput");
 let colRBtn = document.getElementById("shapeColR");
 colRBtn.addEventListener("click", function() {
-  colRVal = colRValIn.value;
+	colRVal = colRValIn.value;
 });
 
 let colGValIn = document.getElementById("shapeColGInput");
 let colGBtn = document.getElementById("shapeColG");
 colGBtn.addEventListener("click", function() {
-  colGVal = colGValIn.value;
+	colGVal = colGValIn.value;
 });
 
 let colBValIn = document.getElementById("shapeColBInput");
 let colBBtn = document.getElementById("shapeColB");
 colBBtn.addEventListener("click", function() {
-  colBVal = colBValIn.value;
+	colBVal = colBValIn.value;
 });
 
 
 function setBoxPosZ(box, step, posZVal) {
   
-  let value = posZVal;
-  let x = box.position.x;
-  let y = box.position.y;
-  let md = abs(x) + abs(y);
-  let d = sqrt(x*x + y*y);
-  let cos = Math.cos;
+	let value = posZVal;
+	let x = box.position.x;
+	let y = box.position.y;
+	let md = abs(x) + abs(y);
+	let d = sqrt(x*x + y*y);
+	let cos = Math.cos;
   
-  box.position.z = eval(value);
+	box.position.z = eval(value);
   
 }
 
 function setBoxScaleZ(box, step, scaleZVal) {
   
-  let value = scaleZVal;
-  let x = box.position.x;
-  let y = box.position.y;
-  let md = abs(x) + abs(y);
-  let d = sqrt(x*x + y*y);
+	let value = scaleZVal;
+	let x = box.position.x;
+	let y = box.position.y;
+	let md = abs(x) + abs(y);
+	let d = sqrt(x*x + y*y);
   
-  box.scale.z = eval(scaleZVal);
+	box.scale.z = eval(scaleZVal);
   
 }
 
 function setBoxScaleX(box, step, scaleXVal) {
   
-  let value = scaleXVal;
-  let x = box.position.x;
-  let y = box.position.y;
-  let md = abs(x) + abs(y);
-  let d = sqrt(x*x + y*y);
+	let value = scaleXVal;
+	let x = box.position.x;
+	let y = box.position.y;
+	let md = abs(x) + abs(y);
+	let d = sqrt(x*x + y*y);
   
-  box.scale.x = eval(scaleXVal);
+	box.scale.x = eval(scaleXVal);
   
 }
 
 function setBoxScaleY(box, step, scaleYVal) {
   
-  let value = scaleYVal;
-  let x = box.position.x;
-  let y = box.position.y;
-  let md = abs(x) + abs(y);
-  let d = sqrt(x*x + y*y);
+	let value = scaleYVal;
+	let x = box.position.x;
+	let y = box.position.y;
+	let md = abs(x) + abs(y);
+	let d = sqrt(x*x + y*y);
   
-  box.scale.y = eval(scaleYVal);
+	box.scale.y = eval(scaleYVal);
   
 }
 
 function setBoxRotationZ(box, step, rotationZVal) {
   
-  let value = rotationZVal;
-  let x = box.position.x;
-  let y = box.position.y;
-  let md = abs(x) + abs(y);
-  let d = sqrt(x*x + y*y);
+	let value = rotationZVal;
+	let x = box.position.x;
+	let y = box.position.y;
+	let md = abs(x) + abs(y);
+	let d = sqrt(x*x + y*y);
   
-  box.rotation.z = eval(rotationZVal);
+	box.rotation.z = eval(rotationZVal);
   
 }
 
 function setBoxRotationX(box, step, rotationXVal) {
   
-  let value = rotationXVal;
-  let x = box.position.x;
-  let y = box.position.y;
-  let md = abs(x) + abs(y);
-  let d = sqrt(x*x + y*y);
+	let value = rotationXVal;
+	let x = box.position.x;
+	let y = box.position.y;
+	let md = abs(x) + abs(y);
+	let d = sqrt(x*x + y*y);
   
-  box.rotation.x = eval(rotationXVal);
+	box.rotation.x = eval(rotationXVal);
   
 }
 
 function setBoxRotationY(box, step, rotationYVal) {
   
-  let value = rotationYVal;
-  let x = box.position.x;
-  let y = box.position.y;
-  let md = abs(x) + abs(y);
-  let d = sqrt(x*x + y*y);
+	let value = rotationYVal;
+	let x = box.position.x;
+	let y = box.position.y;
+	let md = abs(x) + abs(y);
+	let d = sqrt(x*x + y*y);
   
-  box.rotation.y = eval(rotationYVal);
+	box.rotation.y = eval(rotationYVal);
   
 }
 
 
 function setBoxColorR(box, step, colRVal) {
   
-  let value = colRVal;
-  let x = box.position.x;
-  let y = box.position.y;
-  let md = abs(x) + abs(y);
-  let d = sqrt(x*x + y*y);
+	let value = colRVal;
+	let x = box.position.x;
+	let y = box.position.y;
+	let md = abs(x) + abs(y);
+	let d = sqrt(x*x + y*y);
   
-  box.material.color.r = eval(value);
+	box.material.color.r = eval(value);
 }
 
 function setBoxColorG(box, step, colGVal) {
   
-  let valueG = colGVal;
-  let x = box.position.x;
-  let y = box.position.y;
-  let md = abs(x) + abs(y);
-  let d = sqrt(x*x + y*y);
+	let valueG = colGVal;
+	let x = box.position.x;
+	let y = box.position.y;
+	let md = abs(x) + abs(y);
+	let d = sqrt(x*x + y*y);
   
-  box.material.color.g = eval(valueG);
+	box.material.color.g = eval(valueG);
 }
 
 function setBoxColorB(box, step, colBVal) {
   
-  let valueB = colBVal;
-  let x = box.position.x;
-  let y = box.position.y;
-  let md = abs(x) + abs(y);
-  let d = sqrt(x*x + y*y);
+	let valueB = colBVal;
+	let x = box.position.x;
+	let y = box.position.y;
+	let md = abs(x) + abs(y);
+	let d = sqrt(x*x + y*y);
   
-  box.material.color.b = eval(valueB);
+	box.material.color.b = eval(valueB);
 }
 
 
@@ -234,73 +235,73 @@ let shapeSelector = document.getElementById("shapeSelector");
 shapeSelector.addEventListener("change", () => setShape());
 
 function setShape() {
-  switch (shapeSelector.value) {
+	switch (shapeSelector.value) {
       
-    case "box":
-    boxes.forEach(cube => {
-      cube.geometry.dispose();
-      geometry = new THREE.BoxGeometry(1, 1, 1);
-      cube.geometry = geometry.clone();
-    });
-    break;
+	case "box":
+		boxes.forEach(cube => {
+			cube.geometry.dispose();
+			geometry = new THREE.BoxGeometry(1, 1, 1);
+			cube.geometry = geometry.clone();
+		});
+		break;
       
-    case "ring":
-      boxes.forEach(cube => {
-      cube.geometry.dispose();
-      geometry = new THREE.RingGeometry( 1, 5, 6 );
-      cube.geometry = geometry.clone();
-    });
-    break;
+	case "ring":
+		boxes.forEach(cube => {
+			cube.geometry.dispose();
+			geometry = new THREE.RingGeometry( 1, 5, 6 );
+			cube.geometry = geometry.clone();
+		});
+		break;
       
-    case "dodecahedron":
-      boxes.forEach(cube => {
-      cube.geometry.dispose();
-      geometry = new THREE.DodecahedronGeometry(1, 3);
-      cube.geometry = geometry.clone();
-    });
-    break;
+	case "dodecahedron":
+		boxes.forEach(cube => {
+			cube.geometry.dispose();
+			geometry = new THREE.DodecahedronGeometry(1, 3);
+			cube.geometry = geometry.clone();
+		});
+		break;
       
-    case "dodecahedron-large":
-      boxes.forEach(cube => {
-      cube.geometry.dispose();
-      geometry = new THREE.DodecahedronGeometry(3, 1);
-      cube.geometry = geometry.clone();
-    });
-    break;
+	case "dodecahedron-large":
+		boxes.forEach(cube => {
+			cube.geometry.dispose();
+			geometry = new THREE.DodecahedronGeometry(3, 1);
+			cube.geometry = geometry.clone();
+		});
+		break;
       
-    case "cylinder":
-      boxes.forEach(cube => {
-      cube.geometry.dispose();
-      geometry = new THREE.CylinderGeometry(1, 1, 10, 12);
-      cube.geometry = geometry.clone();
-    });
-    break;
+	case "cylinder":
+		boxes.forEach(cube => {
+			cube.geometry.dispose();
+			geometry = new THREE.CylinderGeometry(1, 1, 10, 12);
+			cube.geometry = geometry.clone();
+		});
+		break;
       
-    case "plane":
-      boxes.forEach(cube => {
-      cube.geometry.dispose();
-      geometry = new THREE.PlaneGeometry(1, 1, 1);
-      cube.geometry = geometry.clone();
-    });
-    break;
+	case "plane":
+		boxes.forEach(cube => {
+			cube.geometry.dispose();
+			geometry = new THREE.PlaneGeometry(1, 1, 1);
+			cube.geometry = geometry.clone();
+		});
+		break;
     
-    case "octahedron":
-      boxes.forEach(cube => {
-      cube.geometry.dispose();
-      geometry = new THREE.OctahedronGeometry(1, 0);
-      cube.geometry = geometry.clone();
-    });
-    break;
+	case "octahedron":
+		boxes.forEach(cube => {
+			cube.geometry.dispose();
+			geometry = new THREE.OctahedronGeometry(1, 0);
+			cube.geometry = geometry.clone();
+		});
+		break;
       
-      case "octahedron-large":
-      boxes.forEach(cube => {
-      cube.geometry.dispose();
-      geometry = new THREE.OctahedronGeometry(3, 0);
-      cube.geometry = geometry.clone();
-    });
-    break;
+	case "octahedron-large":
+		boxes.forEach(cube => {
+			cube.geometry.dispose();
+			geometry = new THREE.OctahedronGeometry(3, 0);
+			cube.geometry = geometry.clone();
+		});
+		break;
   
-}
+	}
 }
 
 // camera position slider
@@ -310,8 +311,8 @@ cameraPosZoutput.innerHTML = cameraPosZslider.value; // Display the default slid
 
 // Update the current slider value (each time you drag the slider handle)
 cameraPosZslider.oninput = function () {
-  cameraPosZoutput.innerHTML = this.value;
-  camera.position.z = parseFloat(this.value);
+	cameraPosZoutput.innerHTML = this.value;
+	camera.position.z = parseFloat(this.value);
   
 };
 
@@ -322,39 +323,39 @@ let rotationX = document.getElementById("rotateX");
 rotationX.addEventListener("click", () => rotation = true);
 
 function updateBoxes(step) {
-  for (let i = 0; i < boxes.length; i++) {
+	for (let i = 0; i < boxes.length; i++) {
     
-    setBoxPosZ(boxes[i], step, posZVal);
-    setBoxScaleZ(boxes[i], step, scaleZVal);
-    setBoxScaleX(boxes[i], step, scaleXVal);
-    setBoxScaleY(boxes[i], step, scaleYVal);
-    setBoxRotationZ(boxes[i], step, rotationZVal);
-    setBoxRotationX(boxes[i], step, rotationXVal);
-    setBoxRotationY(boxes[i], step, rotationYVal);
-    setBoxColorR(boxes[i], step, colRVal);
-    setBoxColorG(boxes[i], step, colGVal);
-    setBoxColorB(boxes[i], step, colBVal);
+		setBoxPosZ(boxes[i], step, posZVal);
+		setBoxScaleZ(boxes[i], step, scaleZVal);
+		setBoxScaleX(boxes[i], step, scaleXVal);
+		setBoxScaleY(boxes[i], step, scaleYVal);
+		setBoxRotationZ(boxes[i], step, rotationZVal);
+		setBoxRotationX(boxes[i], step, rotationXVal);
+		setBoxRotationY(boxes[i], step, rotationYVal);
+		setBoxColorR(boxes[i], step, colRVal);
+		setBoxColorG(boxes[i], step, colGVal);
+		setBoxColorB(boxes[i], step, colBVal);
 
-  }
+	}
 }
 
 let step = 5;
 
 var animate = function () {
   
-  requestAnimationFrame( animate );
-  step += 0.01;
-  updateBoxes (step);
-  renderer.render( scene, camera );
+	requestAnimationFrame( animate );
+	step += 0.01;
+	updateBoxes (step);
+	renderer.render( scene, camera );
   
-  if(rotation) {
-  // Use Math.cos and Math.sin to set camera X and Z values based on angle. 
-camera.position.x = radius * Math.cos( angle ); 
-camera.position.z = radius * Math.sin( angle );
-camera.position.y = radius * Math.sin( angle );
+	if(rotation) {
+		// Use Math.cos and Math.sin to set camera X and Z values based on angle. 
+		camera.position.x = radius * Math.cos( angle ); 
+		camera.position.z = radius * Math.sin( angle );
+		camera.position.y = radius * Math.sin( angle );
   
-angle += 0.005;
-  }
+		angle += 0.005;
+	}
 };
 
 animate();
